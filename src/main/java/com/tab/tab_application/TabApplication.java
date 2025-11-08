acb89a2d3baa263b56f918ad9f357b56b5e20c48
tran-daniel4 <tdanielhuynh@gmail.com>
2025-11-08 12:49:39 -0500

    initialized springboot project

---
+++ src/main/java/com/tab/tab_application/TabApplication.java
@@ -0,0 +1,13 @@
+package com.tab.tab_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tab/tab_application/TabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tab.tab_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
