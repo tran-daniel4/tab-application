1f39a37c9d38acd7b76e4500a1f0932477ca15ed
tran-daniel4 <tdanielhuynh@gmail.com>
2026-01-14 22:58:38 -0500

    added controller endpoints for receipt

--- src/main/java/com/tab/tab_application/receipt/controller/ReceiptController.java
+++ src/main/java/com/tab/tab_application/receipt/controller/ReceiptController.java
@@ -1,14 +1,49 @@
 package com.tab.tab_application.receipt.controller;
 
+import com.tab.tab_application.receipt.dto.ReceiptRequestDTO;
+import com.tab.tab_application.receipt.dto.ReceiptResponseDTO;
 import com.tab.tab_application.receipt.service.ReceiptService;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
+@RequestMapping("/api/receipt")
 public class ReceiptController {
     private final ReceiptService receiptService;
 
     public ReceiptController(ReceiptService receiptService) {
         this.receiptService = receiptService;
     }
+
+    @PostMapping
+    public ResponseEntity<ReceiptResponseDTO> createReceipt(@Valid @RequestBody ReceiptRequestDTO request) {
+        ReceiptResponseDTO created = receiptService.createReceipt(request);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ReceiptResponseDTO> getReceiptById(@PathVariable Long id) {
+        ReceiptResponseDTO receipt = receiptService.getReceiptById(id);
+        return ResponseEntity.ok(receipt);
+    }
+
+    @PostMapping
+    public ResponseEntity<ReceiptResponseDTO> updateReceipt(@PathVariable Long id, @Valid @RequestBody ReceiptRequestDTO request) {
+        ReceiptResponseDTO updated = receiptService.updateReceipt(id, request);
+        return ResponseEntity.ok(updated);
+    }
+
+
 
 }
